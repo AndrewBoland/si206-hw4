7d942d1f67feaa9c642fa140c3fc63130445ebf2
andrewbo <andrewbo@umich.edu>
2017-02-07 09:45:26 -0500

    Finished part 2

--- SI206W17_HW4.py
+++ SI206W17_HW4.py
@@ -45,8 +45,19 @@ fileref.close()
 ## HINT: Remember that you'll need to open the file you created in Part 1, read the contets into one big string, and make a BeautifulSoup object out of that string!
 ## NOTE that the provided link does not include saving the online data in a file as part of the process. But it still provides very useful hints/tricks about how to look for and identify the headlines on the NY Times page.
 
+fileref = open("nytimes.html", "r")
+text = fileref.read()
+soup = BeautifulSoup(text, "html.parser")
+nytimes_headlines = []
+for index, story_heading in enumerate(soup.find_all(class_="story-heading")):
+    if index == 10:
+        break
+    if story_heading.a:
+        nytimes_headlines.append(story_heading.a.text.replace("\n", " ").strip())
+    else:
+        nytimes_headlines.append(story_heading.contents[0].strip())
 
-
+fileref.close()
 
 #####################
 
